73b026b7dbd5717877d580ab8f78d4d5b21c4ece
sahasourav17 <sahasourav1170@gmail.com>
2024-04-05 06:58:21 +0600

    add cors to the server

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,17 @@ const { health } = require("./routes/health");
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 const app = express();
 dotenv.config();
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/health", health);
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
-app.use("/user",userRoutes)
+app.use("/user", userRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
